4f6e2f3b20a586a92af9f440acd83f0b2b7eef09
Tajvir Chahal <tajvirchahal87@gmail.com>
2024-03-31 16:29:18 -0400

    Added Coffee View page and back buttons

--- src/main/java/com/example/softmethproject4new/HelloApplication.java
+++ src/main/java/com/example/softmethproject4new/HelloApplication.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     private Scene scene1;
     private Scene scene2;
+    private Scene coffeeScene;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -19,9 +20,20 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("donut-view.fxml"));
         scene2 = new Scene(fxmlLoader2.load(), 700, 500);
 
+        FXMLLoader fxmlLoader3 = new FXMLLoader(HelloApplication.class.getResource("coffee-view.fxml"));
+        coffeeScene = new Scene(fxmlLoader3.load(), 700, 500);
+
         // Set the controller for the loaded FXML file and pass the scenes and stage to it
         HelloController controller = fxmlLoader1.getController();
-        controller.initScenesAndStage(scene1, scene2, stage);
+        controller.initScenesAndStage(scene1, scene2, coffeeScene, stage);
+
+        HelloController controller2 = fxmlLoader2.getController();
+        controller2.initScenesAndStage(scene1, scene2, coffeeScene, stage);
+
+        HelloController controller3 = fxmlLoader3.getController();
+        controller3.initScenesAndStage(scene1, scene2, coffeeScene, stage);
+
+
 
         stage.setTitle("Hello!");
         stage.setScene(scene1);
--- src/main/java/com/example/softmethproject4new/HelloController.java
+++ src/main/java/com/example/softmethproject4new/HelloController.java
@@ -15,21 +15,40 @@ public class HelloController {
     private Scene scene1;
     private Scene scene2;
     private Stage primaryStage;
+    private Scene coffeeScene;
 
     // Method to initialize the scenes and stage
-    public void initScenesAndStage(Scene scene1, Scene scene2, Stage primaryStage) {
+    public void initScenesAndStage(Scene scene1, Scene scene2, Scene coffeeScene, Stage primaryStage) {
         this.scene1 = scene1;
         this.scene2 = scene2;
+        this.coffeeScene = coffeeScene;
         this.primaryStage = primaryStage;
     }
 
     @FXML
     protected void onHelloButtonClick() throws IOException {
         // Change the welcome text
-        welcomeText.setText("Welcome to JavaFX Application!");
-        System.out.println("Button clicked!");
+        //welcomeText.setText("Welcome to JavaFX Application!");
 
         // Switch scenes
         primaryStage.setScene(scene2);
+    }
+
+    @FXML
+    protected void moveToCoffeeView() throws IOException {
+        // Change the welcome text
+        //welcomeText.setText("Welcome to JavaFX Application!");
+
+        // Switch scenes
+        primaryStage.setScene(coffeeScene);
+    }
+
+    @FXML
+    protected void backToMain() throws IOException {
+        // Change the welcome text
+        //welcomeText.setText("Welcome to JavaFX Application!");
+
+        // Switch scenes
+        primaryStage.setScene(scene1);
     }
 }
