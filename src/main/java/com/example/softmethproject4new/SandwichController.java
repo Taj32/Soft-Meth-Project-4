a769fae01c1ecde8ee2e7d8c935c90a11fc4e721
Tajvir Chahal <tajvirchahal87@gmail.com>
2024-04-02 17:27:50 -0400

    Controllers should be setup for all pages

---
+++ src/main/java/com/example/softmethproject4new/SandwichController.java
@@ -0,0 +1,39 @@
+package com.example.softmethproject4new;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+public class SandwichController {
+    private MainController mainController;
+    private Stage stage;
+    private Scene primaryScene;
+    private Stage primaryStage;
+    private ObservableList<String> colorList;
+    private ObservableList<String> fruitList;
+    @FXML
+    private Label value;
+    @FXML
+    private Label color;
+    @FXML
+    private ComboBox<String> cmb_color;
+    @FXML
+    private ListView<String> listview;
+    @FXML
+    private Button menuButton;
+
+    public void setMainController (MainController controller,
+                                   Stage stage,
+                                   Stage primaryStage,
+                                   Scene primaryScene) {
+        mainController = controller;
+        this.stage = stage;
+        this.primaryStage = primaryStage;
+        this.primaryScene = primaryScene;
+    }
+}
